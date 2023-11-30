65e9dd02570102ee9b1155e003eb6554758beb0b
moiwi <71585945+moiwi@users.noreply.github.com>
2023-11-30 20:37:22 +0100

    fix for gcc 11 with c++20

--- include/fmt/format.h
+++ include/fmt/format.h
@@ -2005,9 +2005,9 @@ FMT_CONSTEXPR20 auto write(OutputIt out, T value,
   fspecs.sign = specs.sign;
 
   if (specs.type == presentation_type::fixed_lower) {
-    static const double prevPowerOfTen[17] = {1e-1, 1,    1e1,  1e2,  1e3, 1e4,
-                                              1e5,  1e6,  1e7,  1e8,  1e9, 1e10,
-                                              1e11, 1e12, 1e13, 1e14, 1e15};
+    constexpr double prevPowerOfTen[17] = {1e-1, 1,    1e1,  1e2,  1e3, 1e4,
+                                           1e5,  1e6,  1e7,  1e8,  1e9, 1e10,
+                                           1e11, 1e12, 1e13, 1e14, 1e15};
     if (specs.precision < 0) {
       specs.precision = 6;
     }
